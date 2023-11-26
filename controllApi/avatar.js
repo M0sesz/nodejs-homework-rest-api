a3a74acad5bcf3257f5a22cc6bfef0871bbbd2ce
Anton <rodrigescouy25@gmail.com>
2023-11-26 18:18:23 +0200

    v1.1

--- controllApi/avatar.js
+++ controllApi/avatar.js
@@ -1,31 +1,37 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 const jimp = require("jimp");
-
 const { User } = require("../models/user");
 
 const uploadAvatar = async (req, res, next) => {
   try {
+    if (!req.file) {
+      throw new Error("No file attached");
+    }
+
     const image = await jimp.read(req.file.path);
-
-    await image.resize(250, 250);
-
-    await image.writeAsync(req.file.path);
+    await image.cover(250, 250).writeAsync(req.file.path);
 
     await fs.rename(
       req.file.path,
       path.join(__dirname, "../public/avatars", req.file.filename)
     );
 
+    const avatarURL = `/avatars/${req.file.filename}`;
+
     const user = await User.findByIdAndUpdate(
       req.user.id,
-      { avatarURL: req.file.filename },
+      { avatarURL },
       { new: true }
     ).exec();
 
-    res.send({ avatarURL: user.avatarURL });
+    res.json({ avatarURL: user.avatarURL });
   } catch (error) {
-    next(error);
+    if (error.message === "No file attached") {
+      res.send({ message: "Прикріпіть зображення !" });
+    } else {
+      next(error);
+    }
   }
 };
 
